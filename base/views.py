2f61ae911c66de8294348523c7a8ee4d08236327
Jimmy <jimmy.hotchkiss@gmail.com>
2022-05-20 06:13:13 -0700

    First commit for Django learning project.

---
+++ base/views.py
@@ -0,0 +1,24 @@
+from urllib import response
+from django.shortcuts import render
+from .models import Room
+
+
+# Dummy data
+# rooms = [
+#     {'id':1, 'name':'lets learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developers'}
+#     ]
+# Create your views here.
+def home(request):
+    # We can create a variable to pass our dictionary in as a variable
+    # We can pull objects from the db
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # print(room)
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
