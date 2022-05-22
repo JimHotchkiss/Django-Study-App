2c05d710dd2411e61c3e3e2274accf2957c67f13
Jimmy <jimmy.hotchkiss@gmail.com>
2022-05-22 06:15:21 -0700

    Added room_form to template, view and urls.

--- base/views.py
+++ base/views.py
@@ -22,3 +22,8 @@ def room(request, pk):
     # print(room)
     context = {'room':room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+
+    context = {}
+    return render(request, 'base/room_form.html', context)
