31ab87adc723e4a93556a420f5509b06d7f6d1c8
Jimmy <jimmy.hotchkiss@gmail.com>
2022-05-23 05:23:21 -0700

    Added form template and populated meta fields with model attributes.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from urllib import response
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 
 # Dummy data
@@ -24,6 +25,6 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
-
-    context = {}
+    form = RoomForm()
+    context = { 'form': form }
     return render(request, 'base/room_form.html', context)
