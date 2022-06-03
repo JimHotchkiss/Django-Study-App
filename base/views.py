58a9a10432cbca483654d6ca5c40efaf93affa16
Jimmy <jimmy.hotchkiss@gmail.com>
2022-06-03 07:39:37 -0400

    Added user comment functionality.

--- base/views.py
+++ base/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Import our flash message 
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # User login 
@@ -86,8 +86,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
     # print(room)
-    context = {'room':room}
+    context = {'room':room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
