3551862d77ae5e13cf54ce678bf94e620cfd4595
Jimmy <jimmy.hotchkiss@gmail.com>
2022-06-04 07:57:16 -0400

    Add participants in rooms and delete message functionality.

--- base/views.py
+++ base/views.py
@@ -87,6 +87,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -94,9 +95,11 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
+
     # print(room)
-    context = {'room':room, 'room_messages': room_messages}
+    context = {'room':room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -146,3 +149,14 @@ def deleteRoom(request, pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
 
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not authorized to delete message') 
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
